699f690c4c15273162e19d8bd7638e22a3b55c22
Ignacio Casal Quinteiro <icq@gnome.org>
2011-08-06 20:55:22 +0200

    Prepare repository

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="libgit2-glib"
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/src/git2-glib-repository.c) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from GNOME Git (or from"
+    echo "your OS vendor's package manager)."
+    exit 1
+}
+
+USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
