918cec6e028447ddbe68e582a7414230388b53c6
Ignacio Casal Quinteiro <icq@gnome.org>
2011-08-10 19:53:28 +0200

    Add wrapper for blob

---
+++ src/ggit-blob.c
@@ -0,0 +1,103 @@
+/*
+ * ggit-blob.c
+ * This file is part of libgit2-glib
+ *
+ * Copyright (C) 2011 - Ignacio Casal Quinteiro
+ *
+ * libgit2-glib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libgit2-glib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libgit2-glib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+
+#include "ggit-blob.h"
+#include "ggit-object-private.h"
+
+
+G_DEFINE_TYPE (GgitBlob, ggit_blob, GGIT_TYPE_OBJECT)
+
+static void
+ggit_blob_finalize (GObject *object)
+{
+	G_OBJECT_CLASS (ggit_blob_parent_class)->finalize (object);
+}
+
+static void
+ggit_blob_class_init (GgitBlobClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	object_class->finalize = ggit_blob_finalize;
+}
+
+static void
+ggit_blob_init (GgitBlob *self)
+{
+}
+
+GgitBlob *
+_ggit_blob_new (git_blob *blob)
+{
+	GgitBlob *gblob;
+
+	gblob = g_object_new (GGIT_TYPE_BLOB, NULL);
+	GGIT_OBJECT (gblob)->priv->obj = (git_object *)blob;
+
+	return gblob;
+}
+
+/**
+ * ggit_blob_rawcontent:
+ * @blob: a #GgitBlob
+ *
+ * Get a read-only buffer with the raw content of @blob.
+ *
+ * A pointer to the raw content of a blob is returned;
+ * this pointer is owned internally by the object and shall
+ * not be free'd. The pointer may be invalidated at a later
+ * time.
+ *
+ * Returns: (transfer none): the content or %NULL if @blob has no contents
+ */
+gconstpointer
+ggit_blob_rawcontent (GgitBlob *blob)
+{
+	git_blob *b;
+
+	g_return_val_if_fail (GGIT_IS_BLOB (blob), NULL);
+
+	b = (git_blob *)GGIT_OBJECT (blob)->priv->obj;
+
+	return git_blob_rawcontent (b);
+}
+
+/**
+ * ggit_blob_rawsize:
+ * @blob: a #GgitBlob
+ *
+ * Get the size in bytes of the contents of a blob
+ *
+ * Returns: the size on bytes
+ */
+gint
+ggit_blob_rawsize (GgitBlob *blob)
+{
+	git_blob *b;
+
+	g_return_val_if_fail (GGIT_IS_BLOB (blob), 0);
+
+	b = (git_blob *)GGIT_OBJECT (blob)->priv->obj;
+
+	return git_blob_rawsize (b);
+}
---
+++ src/ggit-blob.h
@@ -0,0 +1,65 @@
+/*
+ * ggit-blob.h
+ * This file is part of libgit2-glib
+ *
+ * Copyright (C) 2011 - Ignacio Casal Quinteiro
+ *
+ * libgit2-glib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libgit2-glib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libgit2-glib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef __GGIT_BLOB_H__
+#define __GGIT_BLOB_H__
+
+#include <glib-object.h>
+#include "ggit-object.h"
+#include <git2/blob.h>
+
+G_BEGIN_DECLS
+
+#define GGIT_TYPE_BLOB			(ggit_blob_get_type ())
+#define GGIT_BLOB(obj)			(G_TYPE_CHECK_INSTANCE_CAST ((obj), GGIT_TYPE_BLOB, GgitBlob))
+#define GGIT_BLOB_CONST(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GGIT_TYPE_BLOB, GgitBlob const))
+#define GGIT_BLOB_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST ((klass), GGIT_TYPE_BLOB, GgitBlobClass))
+#define GGIT_IS_BLOB(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GGIT_TYPE_BLOB))
+#define GGIT_IS_BLOB_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GGIT_TYPE_BLOB))
+#define GGIT_BLOB_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GGIT_TYPE_BLOB, GgitBlobClass))
+
+typedef struct _GgitBlob	GgitBlob;
+typedef struct _GgitBlobClass	GgitBlobClass;
+typedef struct _GgitBlobPrivate	GgitBlobPrivate;
+
+struct _GgitBlob {
+	GgitObject parent;
+
+	GgitBlobPrivate *priv;
+};
+
+struct _GgitBlobClass {
+	GgitObjectClass parent_class;
+};
+
+GType             ggit_blob_get_type         (void) G_GNUC_CONST;
+
+GgitBlob        *_ggit_blob_new              (git_blob *blob);
+
+gconstpointer     ggit_blob_rawcontent       (GgitBlob *blob);
+
+gint              ggit_blob_rawsize          (GgitBlob *blob);
+
+G_END_DECLS
+
+#endif /* __GGIT_BLOB_H__ */
