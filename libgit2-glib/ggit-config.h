23e266bce3c306458478427717d29883a73bf0b4
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2012-01-31 23:02:31 +0100

    Added GPL stubs to ggit-config.h

--- libgit2-glib/ggit-config.h
+++ libgit2-glib/ggit-config.h
@@ -1,3 +1,23 @@
+/*
+ * ggit-config.h
+ * This file is part of libggit
+ *
+ * Copyright (C) 2012 - Jesse van den Kieboom
+ *
+ * libggit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libggit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libggit. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GGIT_CONFIG_H__
 #define __GGIT_CONFIG_H__
 
