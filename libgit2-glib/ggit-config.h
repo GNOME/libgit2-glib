6a19e5187b120107e3e5c0e46ecd6b0254147343
Ignacio Casal Quinteiro <icq@gnome.org>
2012-02-14 14:09:59 +0100

    Relicense as LGPL

--- libgit2-glib/ggit-config.h
+++ libgit2-glib/ggit-config.h
@@ -1,21 +1,21 @@
 /*
  * ggit-config.h
- * This file is part of libggit
+ * This file is part of libgit2-glib
  *
  * Copyright (C) 2012 - Jesse van den Kieboom
  *
- * libggit is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * libgit2-glib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * libggit is distributed in the hope that it will be useful,
+ * libgit2-glib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with libggit. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libgit2-glib. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __GGIT_CONFIG_H__
--- libgit2-glib/ggit-repository.c
+++ libgit2-glib/ggit-repository.c
@@ -4,20 +4,18 @@
  *
  * Copyright (C) 2011 - Ignacio Casal Quinteiro
  *
- * libggit is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * libgit2-glib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * libggit is distributed in the hope that it will be useful,
+ * libgit2-glib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with libggit; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libgit2-glib. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <gio/gio.h>
