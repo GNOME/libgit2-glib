4071298e150508d8ce67dd4ca9f05bdd9c8efa25
Ignacio Casal Quinteiro <icq@gnome.org>
2012-10-31 09:51:48 +0100

    LGPL instead of GPL

--- libgit2-glib/ggit-config-entry.c
+++ libgit2-glib/ggit-config-entry.c
@@ -4,17 +4,17 @@
  *
  * Copyright (C) 2012 - Ignacio Casal Quinteiro
  *
- * libgit2-glib is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * libgit2-glib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * libgit2-glib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with libgit2-glib. If not, see <http://www.gnu.org/licenses/>.
  */
 
