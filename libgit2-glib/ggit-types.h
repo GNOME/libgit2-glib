a81ad65ecb2369befcfa957d0ade1453ec35dffd
Jesse van den Kieboom <jessevdk@gnome.org>
2015-08-08 12:08:32 +0200

    Un-line-break flags to make enum generation happier

--- libgit2-glib/ggit-types.h
+++ libgit2-glib/ggit-types.h
@@ -915,8 +915,7 @@ typedef enum {
 	GGIT_DIFF_FIND_COPIES_FROM_UNMODIFIED     = (1u << 3),
 	GGIT_DIFF_FIND_REWRITES                   = (1u << 4),
 	GGIT_DIFF_BREAK_REWRITES                  = (1u << 5),
-	GGIT_DIFF_FIND_AND_BREAK_REWRITES         = (GGIT_DIFF_FIND_REWRITES |
-	                                             GGIT_DIFF_BREAK_REWRITES),
+	GGIT_DIFF_FIND_AND_BREAK_REWRITES         = (GGIT_DIFF_FIND_REWRITES | GGIT_DIFF_BREAK_REWRITES),
 	GGIT_DIFF_FIND_FOR_UNTRACKED              = (1u << 6),
 	GGIT_DIFF_FIND_ALL                        = (0x0ff),
 	GGIT_DIFF_FIND_IGNORE_LEADING_WHITESPACE  = 0,
