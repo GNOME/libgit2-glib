5e9bf6ddd96742209c546aa31dfdc9f6c7211266
Iñigo Martínez <inigomartinez@gmail.com>
2018-11-12 13:29:40 +0100

    meson: Fix DESTDIR use in Windows
    The use of DESTDIR in Windows produces some errors[0]. To fix this
    issue a different approach has been used for paths. Instead of using
    absolute paths, relative paths are used as meson does. One of this
    relative paths is used along with MESON_INSTALL_DESTDIR_PREFIX to
    avoid any issues.
    
    [0] https://github.com/Alexpux/MINGW-packages/pull/4560#issuecomment-431294899

--- meson_vapi_link.py
+++ meson_vapi_link.py
@@ -4,10 +4,7 @@ import os
 import subprocess
 import sys
 
-vapidir = os.path.join(sys.argv[1], 'vala', 'vapi')
-
-destdir = os.environ.get('DESTDIR', '')
-dest_vapidir = os.path.normpath(destdir + os.sep + vapidir)
+vapidir = os.path.join(os.environ['MESON_INSTALL_DESTDIR_PREFIX'], sys.argv[1], 'vala', 'vapi')
 
 # FIXME: meson will not track the creation of these files
 #        https://github.com/mesonbuild/meson/blob/master/mesonbuild/scripts/uninstall.py#L39
@@ -15,7 +12,7 @@ old = 'ggit-1.0'
 new = 'libgit2-glib-1.0'
 
 wd = os.getcwd()
-os.chdir(dest_vapidir)
+os.chdir(vapidir)
 
 for ext in ['vapi', 'deps']:
   src = os.path.join('{}.{}'.format(new, ext))
