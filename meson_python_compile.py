8a1da431dcbf4eda612b5189201f502fbf122726
Alberto Fanjul <albertofanjul@gmail.com>
2019-06-28 18:22:50 +0200

    Use python3

--- meson_python_compile.py
+++ meson_python_compile.py
@@ -5,12 +5,10 @@ import subprocess
 import sys
 
 if not os.environ.get('DESTDIR'):
-  prefix = os.environ['MESON_INSTALL_PREFIX']
-
-  libdir = os.path.join(prefix, sys.argv[1])
+  libdir = sys.argv[1]
 
   print('Byte-compiling python modules...')
-  subprocess.call(['python', '-m', 'compileall', '-f', '-q', libdir])
+  subprocess.call(['python3', '-m', 'compileall', '-f', '-q', libdir])
 
   print('Byte-compiling python modules (optimized versions) ...')
-  subprocess.call(['python', '-O', '-m', 'compileall', '-f', '-q', libdir])
+  subprocess.call(['python3', '-O', '-m', 'compileall', '-f', '-q', libdir])
