272024b9370380a538417004d478ef594bd0703c
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2020-03-01 17:33:12 +0000

    meson_vapi_link.py: Don't reset the cwd
    We're exiting anyway so it's unnecessary.

--- meson_vapi_link.py
+++ meson_vapi_link.py
@@ -5,18 +5,14 @@ import subprocess
 import sys
 
 vapidir = os.path.join(os.environ['MESON_INSTALL_DESTDIR_PREFIX'], sys.argv[1], 'vala', 'vapi')
+os.chdir(vapidir)
 
 # FIXME: meson will not track the creation of these files
 #        https://github.com/mesonbuild/meson/blob/master/mesonbuild/scripts/uninstall.py#L39
 old = 'ggit-1.0'
 new = 'libgit2-glib-1.0'
 
-wd = os.getcwd()
-os.chdir(vapidir)
-
 for ext in ['vapi', 'deps']:
   src = os.path.join('{}.{}'.format(new, ext))
   dest = os.path.join('{}.{}'.format(old, ext))
   subprocess.call(['ln', '-s', '-f', src, dest])
-
-os.chdir(wd)
