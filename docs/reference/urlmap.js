225297f1e24e67946daf2ab082934be3fe84f138
Iñigo Martínez <inigomartinez@gmail.com>
2022-02-04 12:22:15 +0100

    doc: migrate to gi-docgen
    gi-docgen is a new tool that tries to avoid some of the issues of
    gtk-doc and is also faster.
    
    gtk-doc has been replaced by gi-docgen in CI.

---
+++ docs/reference/urlmap.js
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2022 Iñigo Martinez <inigomartinez@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'libgit2', 'https://libgit2.org/libgit2/' ],
+]
